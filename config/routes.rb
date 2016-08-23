92e49d851a669a6fb72536cf817f01916e2172e1
Luan Pham <luanpm88@gmail.com>
2016-08-23 00:31:50 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,21 @@
-# Moved to app/decorators/routes/routes_decorator.rb
+Naturesoft::Menus::Engine.routes.draw do
+  namespace :admin, module: "admin", path: "admin/menus" do
+    resources :menus do
+      collection do
+        put "enable"
+        put "disable"
+        delete 'delete'
+        get "select2"
+        get "params_form"
+      end
+    end
+  end
+  
+  #begin
+  #  # route for menus
+  #  Naturesoft::Menus::Menu.get_all.each do |m|
+  #    get m.url, m.route_params
+  #  end
+  #rescue
+  #end
+end
