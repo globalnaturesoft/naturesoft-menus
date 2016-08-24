095886d1ea8af32b8ce329090ca468fd76d8c435
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-08-23 20:40:40 -0700

    update module for menus

---
+++ app/decorators/models/nsmodule_decorator.rb
@@ -0,0 +1,18 @@
+Naturesoft::Nsmodule.class_eval do
+  @menus = {
+    "menu_box" => {
+      "label" => "Menu box in footer",
+      "options" => {
+        "parent_id" => nil,
+        "deep" => nil,
+      }    
+    },
+    "menu_list" => {
+      "label" => "Menu list in sidebar",
+      "options" => {
+        "parent_id" => nil,
+        "deep" => nil,
+      }    
+    }
+  }
+end
