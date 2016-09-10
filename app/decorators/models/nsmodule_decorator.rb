1bb473049ffece19ca20e25b6add798578f38c16
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-09-09 20:26:15 -0700

    update

--- app/decorators/models/nsmodule_decorator.rb
+++ app/decorators/models/nsmodule_decorator.rb
@@ -1,18 +1,12 @@
 Naturesoft::Nsmodule.class_eval do
   @menus = {
-    "menu_box" => {
-      "label" => "Menu box in footer",
+    "menu" => {
+      "label" => "Menu",
       "options" => {
         "parent_id" => nil,
         "deep" => nil,
+        "style" => "default"
       }    
     },
-    "menu_list" => {
-      "label" => "Menu list in sidebar",
-      "options" => {
-        "parent_id" => nil,
-        "deep" => nil,
-      }    
-    }
   }
 end
