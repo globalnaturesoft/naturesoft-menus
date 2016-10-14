e6e6887cdc5df0cbdd975b1e041d4972fbf56960
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-10-13 18:57:09 -0700

    update setting (thumbnail + item per page) for menus

---
+++ app/decorators/models/option_decorator.rb
@@ -0,0 +1,13 @@
+Naturesoft::Option.class_eval do
+  @menus = {
+    "items_per_page" => 200,
+    "thumbnails" => {
+      "0" => {
+        "code" => "default",
+        "width" => "1400",
+        "height" => "470",
+        "scale" => "fill"
+      }
+    },
+  }
+end
