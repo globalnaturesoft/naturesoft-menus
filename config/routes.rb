c8cf9de539d310d000e402bbdf4ef3d9f92a66ae
Luan Pham <luanpm88@gmail.com>
2016-11-07 01:09:51 -0800

    fix menu excaption

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,11 @@ Naturesoft::Menus::Engine.routes.draw do
     end
   end
   
-  #begin
+  begin
     # route for menus
     Naturesoft::Menus::Menu.get_all.each do |m|
       get m.url, m.route_params
     end
-  #rescue
-  #end
+  rescue
+  end
 end
